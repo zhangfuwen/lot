3328488e518ed83264bcda38254adcea39c843b2
Dean <zhangfuwen@foxmail.com>
2021-04-07 12:09:39 +0800

    xx

--- start_system.sh
+++ start_system.sh
@@ -2,21 +2,32 @@
 cd $(dirname $0)
 # unset LD_PRELOAD in case termux-exec is installed
 unset LD_PRELOAD
-mount --bind -o ro /dev/ ubuntu-fs/dev
-mount --bind /proc/ ubuntu-fs/proc
-mount --bind /storage/emulated/0/Download ubuntu-fs/root/Download
-mount --bind /storage/emulated/0/Pictures ubuntu-fs/root/Pictures
-mount --bind ubuntu-fs/root /dev/shm 
+#mount -t dev /dev root-fs/dev
+mount -t sysfs /sys root-fs/sys
+mount -t proc /proc root-fs/proc
+mount --rbind /dev root-fs/dev
+
+mount --bind /storage/emulated/0/ root-fs/root/Storage
+#mount --bind root-fs/root /dev/shm
 
 # because some sym links uses this path
-mkdir -p ubuntu-fs/data/data/com.termux
-mount --bind /data/data/com.termux ubuntu-fs/data/data/com.termux
+set -x
+mkdir -p "root-fs$(pwd)"
+mount --bind $(pwd) "root-fs$(pwd)"
+
+mkdir -p "root-fs/system/xbin"
+mkdir -p "root-fs/system/sbin"
+mkdir -p "root-fs/system/bin"
+mount --bind /system/bin root-fs/system/bin
+mount --bind /system/xbin root-fs/system/xbin
+mount --bind /sbin root-fs/system/sbin
+set +x
 
 ## uncomment the following line to have access to the home directory of termux
 #command+=" -b /data/data/com.termux/files/home:/root"
-## uncomment the following line to mount /sdcard directly to / 
+## uncomment the following line to mount /sdcard directly to /
 #command+=" -b /sdcard"
-command="chroot ./ubuntu-fs "
+command="chroot ./root-fs "
 command+=" /usr/bin/env -i"
 command+=" HOME=/root"
 command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
---
+++ stop_system.sh
@@ -0,0 +1,8 @@
+umount root-fs/system/xbin
+umount root-fs/system/bin
+umount root-fs/system/sbin
+umount root-fs/proc
+umount root-fs/sys
+umount root-fs/dev/pts
+umount root-fs/$(pwd)
+umount root-fs/dev
